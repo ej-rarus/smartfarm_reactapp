eabb03994ac2041d987051bd372a42f528b66ae3
ej-rarus <lpl2001@naver.com>
2024-10-07 17:16:18 +0900

    Diary.js 추가

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Diary(){
     return(
-        <div>
-
+        <div className='content-wrap'>
+            <h1 className='page-title'>Diary</h1>
+            <hr style={{border: 'none', height: '2px', backgroundColor: 'gray', width:'13rem', marginTop:"0.5rem"}}/>
+            <p className='page-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </div>
     )
 }
