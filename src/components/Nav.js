2d81a9ac0e769eca57995e1b6fc562d25d58b128
ej-rarus <lpl2001@naver.com>
2024-10-21 15:41:26 +0900

    ui update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,35 +2,34 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import '../App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-
-function Nav({ menuVisible, setMenuVisible }){
+function Nav({ menuVisible, setMenuVisible }) {
     const navigate = useNavigate();
-
 
     const toggleMenu = () => {
         setMenuVisible(!menuVisible); // 상태를 토글
     };
 
-    return(
+    return (
         <div className="nav">
             <div className="main-logo-container">
-                <div className="main-logo" onClick={()=>navigate('/')}>SF-Mark1</div>
+                <div className="main-logo" onClick={() => navigate('/')}>SF-Mark1</div>
             </div>
             <div className="nav-btn-container">
-                <div className="nav-btn" onClick={()=>navigate('/about')}>About</div>
-                <div className="nav-btn" onClick={()=>navigate('/dashboard')}>Dashboard</div>
-                <div className="nav-btn" onClick={()=>navigate('/controlpanel')}>Control</div>
-                <div className="nav-btn" onClick={()=>navigate('/diary')}>Diary</div>
-                <div className="nav-btn" onClick={()=>navigate('/contact')}>Contact</div>
+                <div className="nav-btn" onClick={() => navigate('/about')}>About</div>
+                <div className="nav-btn" onClick={() => navigate('/dashboard')}>Dashboard</div>
+                <div className="nav-btn" onClick={() => navigate('/controlpanel')}>Control</div>
+                <div className="nav-btn" onClick={() => navigate('/diary')}>Diary</div>
+                <div className="nav-btn" onClick={() => navigate('/contact')}>Contact</div>
 
-                <div id="burger-menu-btn" 
-                     onClick={toggleMenu}>
+                <div id="burger-menu-btn" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faBars} size="2x" /> {/* 버거 아이콘 */}
                 </div>
             </div>
         </div>
     );
 }
-
 
 export default Nav;
--- src/pages/DiaryNewPost.js
+++ src/pages/DiaryNewPost.js
@@ -25,21 +25,43 @@ function DiaryNewPost() {
   };
 
   return (
-    <div className="new-post-form">
-      <h1>새 글 작성</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>제목:</label>
+    <div className="content-wrap">
+      <h1 className="page-title">새 글 작성</h1>
+      <form className="std-form" onSubmit={handleSubmit}>
+        <div className="std-form-container">
+          <label>제목</label>
           <input
+            className="input-field"
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label>카테고리:</label>
+        
+        <div className="std-form-container">
+          <label>글쓴이</label>
+          <input
+            className="input-field"
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+        </div>
+        <div className="std-form-container">
+          <label>내용</label>
+          <textarea
+            className="input-field"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <div className="std-form-container">
+          <label>카테고리</label>
           <select
+            className="input-field"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
             required
@@ -52,24 +74,9 @@ function DiaryNewPost() {
             <option value="0005">기타</option>
           </select>
         </div>
-        <div>
-          <label>글쓴이:</label>
-          <input
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>내용:</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            required
-          ></textarea>
-        </div>
-        <button type="submit">작성</button>
+        <button className="std-btn" type="submit">작성</button>
+        <button className="std-btn" type="cancel">취소</button>
+
       </form>
     </div>
   );
